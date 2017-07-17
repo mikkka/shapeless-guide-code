9e26706848f701378eb5b6a39cff79e55b5981bd
mick <mick@z-inc.net>
2017-07-17 22:51:36 +0500

    Second and NTH types

---
+++ common/src/main/scala/NTH.scala
@@ -0,0 +1,44 @@
+import shapeless.{::, HList, HNil}
+
+trait NTH[L <: HList] {
+  type Out
+  def apply(value: L): Out
+}
+
+trait NTHn {
+  type Aux[L <: HList, O] = NTH[L] {type Out = O}
+
+  def apply[L <: HList](implicit inst: NTH[L]): Aux[L, inst.Out] = inst
+}
+
+object NTH1 extends NTHn {
+  implicit def idx[A, Rest <: HList]: Aux[A :: Rest, A] =
+    new NTH[A :: Rest] {
+      type Out = A
+      def apply(value: A :: Rest): A = value.head
+    }
+}
+
+object NTH2 extends NTHn {
+  implicit def idx[A, B, Rest <: HList]: Aux[A :: B :: Rest, B] =
+    new NTH[A :: B :: Rest] {
+      type Out = B
+      def apply(value: A :: B :: Rest): B = value.tail.head
+    }
+}
+
+
+object MainNTH extends Demo {
+
+  def n1 = {
+    import NTH1._
+    NTH1[Int :: Int :: HNil]
+  }
+  def n2 = {
+    import NTH2._
+    NTH2[String :: String :: HNil]
+  }
+
+  println(n1(12 :: 13 :: HNil))
+  println(n2("foo" :: "bar" :: HNil))
+}
---
+++ common/src/main/scala/Second.scala
@@ -0,0 +1,27 @@
+import shapeless.{::, HList, HNil}
+
+trait Second[L <: HList] {
+  type Out
+  def apply(value: L): Out
+}
+
+object Second {
+  type Aux[L <: HList, O] = Second[L] {type Out = O}
+
+  def apply[L <: HList](implicit inst: Second[L]): Aux[L, inst.Out] = inst
+
+  implicit def hlistSecond[A, B, Rest <: HList]: Aux[A :: B :: Rest, B] =
+    new Second[A :: B :: Rest] {
+      type Out = B
+      def apply(value: A :: B :: Rest): B = value.tail.head
+    }
+}
+
+
+object MainSecond extends Demo {
+  val sec1 = Second[Int :: Int :: HNil]
+  val sec2 = Second[String :: String :: HNil]
+
+  println(sec1(12 :: 13 :: HNil))
+  println(sec2("foo" :: "bar" :: HNil))
+}
