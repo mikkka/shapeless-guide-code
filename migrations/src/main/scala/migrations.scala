64032092d8a9ee1739039f13ad02cdf8ae47e3df
m.tkachev <m.tkachev@tinkoff.ru>
2017-08-22 19:28:13 +0500

    Complete migrations example

--- migrations/src/main/scala/migrations.scala
+++ migrations/src/main/scala/migrations.scala
@@ -11,17 +11,55 @@ trait Migration[A, B] {
   def apply(original: A): B
 }
 
-
-
 object Migration {
   def pure[A, B](func: A => B): Migration[A, B] =
     new Migration[A, B] {
-      def apply(original: A): B =
-        func(original)
+      def apply(original: A): B = func(original)
     }
 
-}
+  implicit class MigrationOps[A](a: A) {
+    def migrateTo[B](implicit migration: Migration[A, B]): B = migration.apply(a)
+  }
 
+  implicit def genericMigration[A, B,
+    ARepr <: HList, BRepr <: HList,
+    Common <: HList, Added <: HList, Unaligned <: HList](
+
+    implicit
+    aGen      : LabelledGeneric.Aux[A, ARepr],
+    bGen      : LabelledGeneric.Aux[B, BRepr],
+    inter     : hlist.Intersection.Aux[ARepr, BRepr, Common],
+    diff      : hlist.Diff.Aux[BRepr, Common, Added],
+    monoid    : Monoid[Added],
+    prepend   : hlist.Prepend.Aux[Added, Common, Unaligned],
+    align     : hlist.Align[Unaligned, BRepr]
+  ): Migration[A, B] =
+    new Migration[A, B] {
+      def apply(a: A): B =
+        bGen.from(align(prepend(monoid.empty, inter(aGen.to(a)))))
+    }
+
+  def createMonoid[A](zero: A)(add: (A, A) => A): Monoid[A] =
+    new Monoid[A] {
+      def empty = zero
+
+      def combine(x: A, y: A): A = add(x, y)
+    }
+
+  implicit val hnilMonoid: Monoid[HNil] = createMonoid[HNil](HNil)((x, y) => HNil)
+
+  implicit def emptyHList[K <: Symbol, H, T <: HList](
+    implicit hMonoid: Lazy[Monoid[H]],
+    tMonoid: Monoid[T]
+  ): Monoid[FieldType[K, H] :: T] =
+
+    createMonoid(
+      field[K](hMonoid.value.empty) :: tMonoid.empty)
+      {(x, y) =>
+//        ???
+        field[K](hMonoid.value.combine(x.head, y.head)) :: tMonoid.combine(x.tail, y.tail)
+      }
+}
 
 
 case class SameA(a: String, b: Int, c: Boolean)
@@ -42,14 +80,11 @@ case class KitchenSinkB(c: Boolean, z: Option[String], a: String)
 
 
 object Main extends Demo {
-  implicit class MigrationOps[A](original: A) {
-    def migrateTo[B](implicit migration: Migration[A, B]): B =
-      migration(original)
-  }
+  import Migration._
 
-  print(SameA("abc", 123, true).migrateTo[SameB])
-  print(DropFieldA("abc", 123, true).migrateTo[DropFieldB])
-  print(AddFieldA("abc").migrateTo[AddFieldB])
-  print(ReorderA("abc", 123).migrateTo[ReorderB])
-  print(KitchenSinkA("abc", 123, true).migrateTo[KitchenSinkB])
+  println(SameA("abc", 123, true).migrateTo[SameB])
+  println(DropFieldA("abc", 123, true).migrateTo[DropFieldB])
+  println(ReorderA("abc", 123).migrateTo[ReorderB])
+  println(AddFieldA("abc").migrateTo[AddFieldB])
+  println(KitchenSinkA("abc", 123, true).migrateTo[KitchenSinkB])
 }
